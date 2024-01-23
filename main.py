2cb48070a603e4e7fdb91f015c23140c86cbfcba
57qy654 <Jude.Pitschka@gmail.com>
2024-01-22 18:39:07 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+# ---------imports---------
+import pygame
+import random
+
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+    MOUSEBUTTONDOWN,
+)
+
+# Initializes the game
+pygame.init()
+
+# ---------Constants---------
+
+
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+
